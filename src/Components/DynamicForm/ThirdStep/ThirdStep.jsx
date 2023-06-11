aacb3faa98916ccb7dbb92b131f5e165f71be7f3
Samuel <sam.saravia2010@gmail.com>
2023-06-11 15:04:44 -0600

    feat: add ThirdStep with styles

--- src/Components/DynamicForm/ThirdStep/ThirdStep.jsx
+++ src/Components/DynamicForm/ThirdStep/ThirdStep.jsx
@@ -1,9 +1,32 @@
-import "./ThirdStep.css"
+import { Col, Container, FloatingLabel, Form, Row } from "react-bootstrap";
+import "./ThirdStep.css";
 
-const ThirdStep =()=>{
-    return(
-        <>Third Step hello</>
-    )
-}
+const ThirdStep = () => {
+  return (
+    <div>
+      <h2>Do you want add any comment?</h2>
+      <Container className="p-5 text-center">
+        <Row className="justify-content-md-center">
+          <Col lg="10">
+            <FloatingLabel
+              controlId="floatingTextarea"
+              label="Comments"
+              className="mb-3"
+            >
+              <Form.Control
+                as="textarea"
+                placeholder="Leave a comment"
+                onChange={(e) => console.log(e.target.value)}
+              ></Form.Control>
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <div className="send-form">
+          <p className="result p-3">View result 🙌</p>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
 export default ThirdStep;
